d37017523da48753cd342c655b9b6e6eb8c2e7a5
azehgideon <azehgideon@gmail.com>
2023-02-20 05:45:22 +0100

    Created backend and frontend

---
+++ frontend/src/test.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+  // Write your code here
+  let socksNum = [];
+  for (let i of n) {
+    const num = ar[i];
+  }
+  const distinctSocks = ar.filter(function (value, index, self) {
+    return self.indexOf(value) === index;
+  });
+  let socks = [distinctSocks.length];
+  for (let i = 0; i < n; i++) {
+    socks[ar[i]];
+  }
+  return distinctSocks.reduce(function (total, num) {
+    return Math.floor(num / 2);
+  }, 0);
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const ar = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
+
+  const result = sockMerchant(n, ar);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
